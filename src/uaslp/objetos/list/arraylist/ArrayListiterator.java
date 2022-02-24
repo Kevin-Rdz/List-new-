3e16ac7d2f77f15cd13940541bc371a12f1cf57d
Kevin-Rdz <evrick09@gmail.com>
2022-02-23 21:06:04 -0600

    añadir ArrayList

--- src/uaslp/objetos/list/arraylist/ArrayListiterator.java
+++ src/uaslp/objetos/list/arraylist/ArrayListiterator.java
@@ -1,4 +1,17 @@
 package uaslp.objetos.list.arraylist;
 
 public class ArrayListiterator {
+    private ArrayList arrayList;
+    private int currentItem;
+    public ArrayListiterator(ArrayList arrayList){
+        this.arrayList = arrayList;
+    }
+    public boolean hasNext(){
+        return currentItem < arrayList.getSize();
+    }
+    public String next(){
+        String data = arrayList.getAt(currentItem);
+        currentItem++;
+        return data;
+    }
 }
